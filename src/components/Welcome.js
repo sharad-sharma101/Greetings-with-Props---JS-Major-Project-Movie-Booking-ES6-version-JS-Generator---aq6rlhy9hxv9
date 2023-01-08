1dff3fec5f0a2c9a56c0913e5ea373ac47dc99a0
Sharad Sharma <106671710+sharad-sharma101@users.noreply.github.com>
2023-01-08 12:30:37 +0000

    k

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Welcome = ({name}) => {
   return (
     <div>
-      <h1>Hey !{name}</h1> 
+      Hey !<h1>{name}</h1> 
       <h2>Welcome to Newton School.</h2>
     </div>
   )
